57c01c93930b9831b435f82e44392cc1ed4c00f9
John Grant <SEI-John@users.noreply.github.com>
2021-05-28 15:44:27 -0400

    Fixed std::string dependancies

--- fvversioncomparator.h
+++ fvversioncomparator.h
@@ -3,34 +3,36 @@
 
 #include <iosfwd>
 #include <vector>
+#include <string>
 
 
 class FvVersionComparator
 {
 public:
 
-	typedef enum {
-		kSame = 0,
-		kDescending = 1,
-		kAscending = -1
-	} ComparatorResult;
+    typedef enum
+    {
+        kSame       = 0,
+        kDescending = 1,
+        kAscending  = -1
+    } ComparatorResult;
 
-	static ComparatorResult CompareVersions(std::string versionA,
-											std::string versionB);
+    static ComparatorResult CompareVersions(std::string versionA,
+                                            std::string versionB);
 
 private:
 
-	FvVersionComparator();
+    FvVersionComparator();
 
-	typedef enum {
-		kNumberType,
-		kStringType,
-		kSeparatorType
-	} CharacterType;
+    typedef enum
+    {
+        kNumberType,
+        kStringType,
+        kSeparatorType
+    } CharacterType;
 
-	static CharacterType TypeOfCharacter(std::string character);
-	static std::vector<std::string> SplitVersionString(std::string version);
-
+    static CharacterType TypeOfCharacter(std::string character);
+    static std::vector<std::string> SplitVersionString(std::string version);
 };
 
 #endif // FVVERSIONCOMPARATOR_H
